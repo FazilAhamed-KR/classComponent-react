b61923c776bf5b9ea34ceb5bd0e010c34243b866
FazilAhamed-KR <110653442+FazilAhamed-KR@users.noreply.github.com>
2024-07-25 10:47:09 +0530

    Add files via upload

---
+++ src/components/UserFinder.js
@@ -0,0 +1,61 @@
+import { Fragment, Component } from "react";
+import Users from "./Users";
+import classes from "./UserFinder.module.css";
+import UsersContext from "../store/users-context";
+import ErrorBoundary from "./ErrorBoundary";
+
+const DUMMY_USERS = [
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
+];
+
+class UserFinder extends Component {
+  static contextType = UsersContext;
+  constructor() {
+    super();
+    this.state = {
+      filteredUsers: DUMMY_USERS,
+      searchTerm: "",
+    };
+  }
+
+  searchChangeHandler(event) {
+    this.setState({
+      searchTerm: event.target.value,
+    });
+  }
+
+  componentDidMount() {
+    this.setState({
+      filteredUsers: this.context.users,
+    });
+  }
+
+  componentDidUpdate(preprops, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: this.context.users.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+    }
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={classes.finder}>
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
+      </Fragment>
+    );
+  }
+}
+
+
+
+export default UserFinder;
